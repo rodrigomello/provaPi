b195201c44c6d35d1a409c86d879a6b1dff4b371
Viviane Bonadia <vivianebonadia@gmail.com>
2012-12-25 13:33:57 -0200

    Confiram o codigo

--- ex1.m
+++ ex1.m
@@ -16,9 +16,27 @@ n2 = b;
 n1([1:x],[y/2:y]) = zeros;
 n2([1:x],[1:y/2]) = zeros;
 
-%mostra as imagens
-subplot(1,2,1), image(n1),title("lena");
-subplot(1,2,2), image(n2),title("babuino");
+#DFT
+n1fft = fft2(n1);
+n2fft = fft2(n2);
 
-#alguem manja fazer dft no octave?
+#soma
+n3 = n1fft  .+ n2fft;
 
+n1fft([1:3],[1:3])
+n2fft([1:3],[1:3])
+n3([1:3],[1:3])
+
+#inversa
+n3ifft = ifft2(n3);
+#O que você pode dizer sobre a parte imaginária da imagem resultante da transformada inversa? Esta de acordo com o esperado?
+
+#mostra as imagens
+subplot(2,3,1), image(l),title("Lena");
+subplot(2,3,2), image(b),title("Babuino");
+subplot(2,3,3), image(n1),title("lena");
+subplot(2,3,4), image(n2),title("babuino");
+subplot(2,3,5), image(real(n3)),title("FFT Lena+Babuino");
+subplot(2,3,6), image(real(n3ifft)),title("IFFT Lena+Babuino");
+
+#O que você conclui dos resultados? A que propriedade da transformada de Fourier você atribui esse resultado?
